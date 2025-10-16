58be1d185031fe3e460d8a38e8acf748d5dfabbd
Emilio Quezada <equezadanavarro@gmail.com>
2025-10-16 10:14:21 +0200

    Refactored version

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,58 +1,66 @@
 import json
 
+
+# --- Function Definitions ---
+
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file from the given path. """
+    with open(file_path, "r", encoding='utf-8') as handle:
+        return json.load(handle)
 
-# ---- Main Script ----
 
-# Load the list of animal dictionaries
+def serialize_animal(animal_obj):
+    """ Takes a single animal dictionary and returns a formatted HTML card string. """
+
+    # Start the HTML card with the opening list item tag
+    html_card = '<li class="cards__item">'
+
+    # Add the title div
+    html_card += f'<div class="card__title">{animal_obj["name"]}</div>'
+
+    # Add the paragraph for all the details
+    html_card += '<p class="card__text">'
+
+    # Add each detail, checking if it exists first
+    if 'diet' in animal_obj['characteristics']:
+        diet = animal_obj['characteristics']['diet']
+        html_card += f'<strong>Diet:</strong> {diet}<br/>'
+
+    if 'locations' in animal_obj and animal_obj['locations']:
+        location = animal_obj['locations'][0]
+        html_card += f'<strong>Location:</strong> {location}<br/>'
+
+    if 'type' in animal_obj['characteristics']:
+        type_ = animal_obj['characteristics']['type']
+        html_card += f'<strong>Type:</strong> {type_}<br/>'
+
+    # Close the paragraph and list item tags
+    html_card += '</p>'
+    html_card += '</li>'
+
+    # Return the complete HTML string for this one card
+    return html_card
+
+
+# --- Main Script ---
+
+# 1. Load the animal data from the JSON file
 animals_data = load_data('animals_data.json')
 
-# 1 - Read html template file
-with open('animals_template.html', 'r') as file:
+# 2. Read the HTML template file
+with open('animals_template.html', 'r', encoding='utf-8') as file:
     template_content = file.read()
 
-# Start with an empty string before the loop
+# 3. Generate the HTML for all animals by looping and calling our function
 animals_info_string = ""
-
 for animal in animals_data:
-    # --- Start building the HTML for one animal ---
+    animals_info_string += serialize_animal(animal)
 
-    # 1. Add the opening <li> tag (the "bread" of our sandwich)
-    animals_info_string += '<li class="cards__item">'
-
-    # 2. Add the title, wrapped in its own <div>
-    animals_info_string += f'<div class="card__title">{animal["name"]}</div>'
-
-    # 3. Add the opening <p> tag to group the details
-    animals_info_string += '<p class="card__text">'
-
-    # 4. Add each detail, now with a <strong> tag around the label
-    if 'diet' in animal['characteristics']:
-        diet = animal['characteristics']['diet']
-        animals_info_string += f'<strong>Diet:</strong> {diet}<br/>'
-
-    if 'locations' in animal and animal['locations']:
-        location = animal['locations'][0]
-        animals_info_string += f'<strong>Location:</strong> {location}<br/>'
-
-    if 'type' in animal['characteristics']:
-        type_ = animal['characteristics']['type']
-        animals_info_string += f'<strong>Type:</strong> {type_}<br/>'
-
-    # 5. Add the closing </p> tag
-    animals_info_string += '</p>'
-
-    # 6. Add the closing </li> tag to finish this animal's card
-    animals_info_string += '</li>'
-
-# 3. Replace the placeholder in the template with our generated string
+# 4. Replace the placeholder in the template with our generated string
 final_html_content = template_content.replace('__REPLACE_ANIMALS_INFO__', animals_info_string)
 
-# 4. Write the final content to a new HTML file
-with open('animals.html', 'w') as file:
+# 5. Write the final content to a new HTML file
+with open('animals.html', 'w', encoding='utf-8') as file:
     file.write(final_html_content)
 
-print("Successfully created!")
+print("Successfully created animals.html!")
